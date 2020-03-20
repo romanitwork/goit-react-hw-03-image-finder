372cd9a3b6c6c25a761358af1d57e81dbd577a65
romanitwork <workromanmail@gmail.com>
2020-03-20 11:45:52 +0200

    Updates

---
+++ precache-manifest.577419881215d131fe9bfd090964d6ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d3fb5d071ee7dc6339def1142e4bda",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "118cc61ae2c6b3a0c106",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.53195ecf.chunk.css"
+  },
+  {
+    "revision": "5c10d95f8ebd471ee977",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.9bdf6382.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.9bdf6382.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "118cc61ae2c6b3a0c106",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.67ba1c8d.chunk.js"
+  },
+  {
+    "revision": "4992c48e99877600891f",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.ad90512f.js"
+  }
+]);
